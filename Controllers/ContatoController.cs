07487171e0c1d13c98f903ca9b11d1aac215149a
Luiz Felipe <lffm1994@gmail.com>
2022-11-13 19:10:40 -0300

    Criando o POST do Deletar e Alterando o menu

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -65,14 +65,36 @@ namespace ProjetoMVC.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-         public IActionResult Detalhes(int id)
+        public IActionResult Detalhes(int id)
         {
             var contato = _context.Contatos.Find(id);
 
             if (contato == null)
-                 return RedirectToAction(nameof(Index));
+                return RedirectToAction(nameof(Index));
 
             return View(contato);
+        }
+
+        public IActionResult Deletar(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+
+            if (contato == null)
+                return RedirectToAction(nameof(Index));
+
+            return View(contato);
+        }
+
+
+        [HttpPost]
+         public IActionResult Deletar(Contato contato)
+        {
+            var contatoBanco = _context.Contatos.Find(contato.Id);
+
+            _context.Contatos.Remove(contatoBanco);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
