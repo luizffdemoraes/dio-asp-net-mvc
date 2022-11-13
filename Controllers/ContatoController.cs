abe91cd11c228a86d4affa09d54886d9e3e269c0
Luiz Felipe <lffm1994@gmail.com>
2022-11-13 18:56:39 -0300

    Criando a página de detalhes

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -63,7 +63,16 @@ namespace ProjetoMVC.Controllers
             _context.SaveChanges();
 
             return RedirectToAction(nameof(Index));
+        }
 
+         public IActionResult Detalhes(int id)
+        {
+            var contato = _context.Contatos.Find(id);
+
+            if (contato == null)
+                 return RedirectToAction(nameof(Index));
+
+            return View(contato);
         }
     }
 }
